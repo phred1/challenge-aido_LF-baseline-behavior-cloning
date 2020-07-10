12055e8f70964bdc2e91b3c28123fdc289f7cfdb
frank-qcd-qk <frank_chude.qian@outlook.com>
2020-07-10 13:48:30 -0400

    critical fix for wrong sklearn dependency

--- duckieTrainer/train.py
+++ duckieTrainer/train.py
@@ -44,17 +44,17 @@ def load_data():
 # -----------------------------------------------------------------------------
 # root mean squared error (rmse) for regression
 def rmse(y_true, y_pred):
-    from keras import backend
+    from tensorflow.keras import backend
     return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
 
 # mean squared error (mse) for regression
 def mse(y_true, y_pred):
-    from keras import backend
+    from tensorflow.keras import backend
     return backend.mean(backend.square(y_pred - y_true), axis=-1)
 
 # coefficient of determination (R^2) for regression
 def r_square(y_true, y_pred):
-    from keras import backend as K
+    from tensorflow.keras import backend
     SS_res = K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
     return (1 - SS_res/(SS_tot + K.epsilon()))
