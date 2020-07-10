d154b3216d1a66555714c37244850c9e39e04f5d
frank-qcd-qk <frank_chude.qian@outlook.com>
2020-07-10 14:21:54 -0400

    downgrade tensorflow version

--- duckieChallenger/helperFncs.py
+++ duckieChallenger/helperFncs.py
@@ -8,23 +8,23 @@ class Validation_Functions:
 
     # root mean squared error (rmse) for regression
     def rmse(self,y_true, y_pred):
-        from keras import backend
+        from tensorflow.keras import backend
         return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
 
     # mean squared error (mse) for regression
     def mse(self,y_true, y_pred):
-        from keras import backend
+        from tensorflow.keras import backend
         return backend.mean(backend.square(y_pred - y_true), axis=-1)
 
     # coefficient of determination (R^2) for regression
     def r_square(self,y_true, y_pred):
-        from keras import backend as K
+        from tensorflow.keras import backend as K
         SS_res =  K.sum(K.square(y_true - y_pred)) 
         SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
         return (1 - SS_res/(SS_tot + K.epsilon()))
 
     def r_square_loss(self,y_true, y_pred):
-        from keras import backend as K
+        from tensorflow.keras import backend as K
         SS_res =  K.sum(K.square(y_true - y_pred)) 
         SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
         return 1 - ( 1 - SS_res/(SS_tot + K.epsilon()))
