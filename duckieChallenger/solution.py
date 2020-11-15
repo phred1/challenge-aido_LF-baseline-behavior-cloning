7d8ed8bf39cfb0016a745f941111824f6e368dc5
frank-qcd-qk <frank_chude.qian@outlook.com>
2020-11-14 20:05:06 -0500

    test gpu restriction

--- duckieChallenger/solution.py
+++ duckieChallenger/solution.py
@@ -33,6 +33,12 @@ class TensorflowTemplateAgent:
                 msg = 'Could not find gpu device.'
                 logger.error(msg)
                 raise Exception(msg)
+            
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        try:
+            tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+        except RuntimeError as e:
+            print(e)
 
     def init(self, context: Context):
         context.info('init()')
