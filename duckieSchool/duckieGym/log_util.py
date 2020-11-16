92f7473146ac0c5392f13ac9bffef71718554af4
frank-qcd-qk <frank_chude.qian@outlook.com>
2020-11-15 19:45:13 -0500

    log_combiner WIP

--- duckieSchool/duckieGym/log_util.py
+++ duckieSchool/duckieGym/log_util.py
@@ -48,7 +48,6 @@ class Logger:
     def close(self):
         self._multithreaded_recording.shutdown()
         self._log_file.close()
-        os.chmod(self._log_file.name, 0o444)  # make file read-only after finishing
 
 class SteeringToWheelVelWrapper:
     """ Converts policy that was trained with [velocity|heading] actions to
